085195e752cc34a1b2fc6dc3d8b25961460dd5d4
bayushi-yakusoku <bayushi.tonio@gmail.com>
2021-01-25 15:41:14 +0100

    adding configuration file

--- springBatchRun.bash
+++ springBatchRun.bash
@@ -4,13 +4,15 @@ JAR_FILE=$(find . -name "SpringBatchHelloWorld*.jar" -printf "%T@#%Tc#%p\n" \
            | sort -n \
            | cut -d# -f3 \
            | tail -1)
+SPRING_PARAMETERS="--spring.config.location=./Config/application.properties"
 JAR_PARAMETERS="fileName=toto.csv name=pouf executionDate(date)=2021/01/18"
 
 echo "Start Java"
-echo "Jar selected : $JAR_FILE"
-echo "Parameters   : $JAR_PARAMETERS"
+echo "Jar selected      : $JAR_FILE"
+echo "spring parameters : $SPRING_PARAMETERS"
+echo "Parameters        : $JAR_PARAMETERS"
 echo
-java -jar $JAR_FILE $JAR_PARAMETERS
+java -jar $JAR_FILE $SPRING_PARAMETERS $JAR_PARAMETERS
 
 echo
 echo "End of script"
